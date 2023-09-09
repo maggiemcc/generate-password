3150159bb2d0ed50bf21c922a43de2c9a9c3948c
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-09 14:39:11 -0600

    added function for uppercase

--- resources/script.js
+++ resources/script.js
@@ -3,6 +3,7 @@
 var generateBtn = document.querySelector("#generate");
 var password = [];
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Write password to the #password input
 function writePassword() {
@@ -15,12 +16,12 @@ function writePassword() {
 function generatePassword() {
   passwordLength();
   passwordLowercase();
-
+  passwordUppercase()
 }
 
   // Asks user for desired password length.
 function passwordLength() {
-  var userLength = window.prompt("Choose your password length. It must be a minumum of 8 and maximum of 128 characters. Ex: 12");
+  var userLength = window.prompt("Choose your password length. \n It must be a minumum of 8 and maximum of 128 characters. \n Ex: 12, 50, 100");
 
   // Checks for correct length
   if (userLength >= 8 && userLength <= 128) {
@@ -30,7 +31,7 @@ function passwordLength() {
     return;
   } else {
     console.log("try again--->", userLength)
-    alert("Please try again, your password was too short or long. Must be between 8 and 128 characters.")
+    alert("Please try again, your password was too short or long. \n Must be between 8 and 128 characters.")
     passwordLength();
   }
 }
@@ -43,7 +44,20 @@ function passwordLowercase() {
     return;
   }
   else {
-    console.log("no lowercase");
+    console.log("Lowercase NOT included", password);
+    return;
+  }
+}
+
+// Asks user if they want uppercase based on if they choose "Okay" or "CANCEL".
+function passwordUppercase() {
+  if (confirm("Would you like lowercase included in your password? \n Choose 'OK' to include uppercase. \n Choose 'Cancel' for no uppercase.")) {
+    password.push(uppercase);
+    console.log("Uppercase included", password);
+    return;
+  }
+  else {
+    console.log("Uppercase NOT included", password);
     return;
   }
 }
